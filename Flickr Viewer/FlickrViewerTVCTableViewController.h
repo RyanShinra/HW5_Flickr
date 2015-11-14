e10399f7d194c483c6861d9bf857216eb83d3891
Ryan Osterday <Ryan@ryans-macbook-pro.local>
2015-11-14 11:07:29 -0500

    First Version, with two Storyboards and a TVC for the table view

---
+++ Flickr Viewer/FlickrViewerTVCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrViewerTVCTableViewController.h
+//  Flickr Viewer
+//
+//  Created by Ryan Osterday on 11/14/15.
+//  Copyright © 2015 Ryan Osterday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrViewerTVCTableViewController : UITableViewController
+
+@end
